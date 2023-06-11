3efa39f825365ca9de9939cfabeea870891cf8bd
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-06-11 12:53:11 -0700

    tekshirildi va ishlandi

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,19 +1,14 @@
 package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
-    public void printMultiplied(int multiplyByAndToInclusive) {
-        int i = 0;
-        int product = 1;
-
-        while (i <= multiplyByAndToInclusive) {
-            if (i < 1) {
-                product = -product;
-            }
-            for (int j = 1; j <= i; j++) {
-                product *= j;
-            }
+    public void printMultiplied(int x) {
+        if(x == 0) return;
+        int  i =0;
+        while (Math.abs(x)>=i){
+            int product =i*x;
             System.out.println(product);
             i++;
+
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int i = 2;
+
+        while (i <= printToInclusive) {
+            boolean z = true;
+            int j = 2;  //i = 6
+            while (j < i) {
+                if (i % j == 0) {
+                    z = false;
+                    break;
+                }
+                j++;
+            }
+            if (z) System.out.println(i);
+            i++;
+        }
+
     }
+
+
 }
